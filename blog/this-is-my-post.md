99569496be1ebc0dd488a8fda853dfed4ec92908
nguyentu1996 <nguyentu19962710@gmail.com>
2022-02-11 14:18:39 +0700

    add scully

---
+++ blog/this-is-my-post.md
@@ -0,0 +1,9 @@
+---
+title: 'This is my post'
+description: 'blog description'
+published: false
+slugs:
+    - ___UNPUBLISHED___kywnu5t6_YzgBeFQAW0f3bGjR5HvawSfTQAcPA52C
+---
+
+# This is my post
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'TestDocker';
+export class AppComponent implements OnInit {
+  name = 'TestDocker';
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {
+
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.name = 'tutest';
+      // this.cdr.markForCheck();
+    }, 5000)
+  }
+
 }
